6a49ef7e18bf41141593ebd458abdb794c126b09
PerterPon <PerterPon@gmail.com>
2013-03-20 00:48:16 +0800

    创建大致雏形

---
+++ GoldUnion/app/app.js
@@ -0,0 +1,65 @@
+﻿Ext.application({
+    name: 'Sencha',
+
+    launch: function() {
+        //The whole app UI lives in this tab panel
+        Ext.Viewport.add({
+            xtype: 'tabpanel',
+            fullscreen: true,
+            tabBarPosition: 'bottom',
+
+            items: [
+                // This is the home page, just some simple html
+                {
+                    xtype: 'nestedlist',
+                    title: '任务列表',
+                    iconCls: 'home',
+                    cls: 'home'
+                },
+
+                // This is the recent blogs page. It uses a tree store to load its data from blog.json
+                {
+                    xtype: 'nestedlist',
+                    title: '我的任务',
+                    iconCls: 'star',
+                    cls: 'blog',
+                    displayField: 'title',
+
+                    store: {
+                        type: 'tree',
+
+                        fields: ['title', 'link', 'author', 'contentSnippet', 'content', {
+                            name: 'leaf',
+                            defaultValue: true
+                        }],
+
+                        root: {
+                            leaf: false
+                        },
+
+                        proxy: {
+                            type: 'jsonp',
+                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'responseData.feed.entries'
+                            }
+                        }
+                    },
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
+                }
+            ]
+        });
+    }
+});
