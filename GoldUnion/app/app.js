0874302d27e2ca74f16d9b242c6b2d3071e26a49
PerterPon <PerterPon@gmail.com>
2013-03-20 23:36:34 +0800

    完成大致的框架

--- GoldUnion/app/app.js
+++ GoldUnion/app/app.js
@@ -2,29 +2,38 @@
     name: 'Sencha',
 
     launch: function() {
-        //The whole app UI lives in this tab panel
         Ext.Viewport.add({
             xtype: 'tabpanel',
             fullscreen: true,
             tabBarPosition: 'bottom',
 
             items: [
-                // This is the home page, just some simple html
                 {
                     xtype: 'nestedlist',
                     title: '任务列表',
-                    iconCls: 'home',
-                    cls: 'home'
-                },
-
-                // This is the recent blogs page. It uses a tree store to load its data from blog.json
-                {
-                    xtype: 'nestedlist',
-                    title: '我的任务',
                     iconCls: 'star',
                     cls: 'blog',
                     displayField: 'title',
-
+                    toolbar : {
+                        xtype : 'toolbar',
+                        layout : 'hbox',
+                        items  : [{
+                            xtype : 'button',
+                            text : '发布任务',
+                            right : '0px',
+                            top : '12%',
+                            height : '60%',
+                            listeners : {
+                                tap : function(){
+                                    var container = Ext.create( 'Ext.Container', {
+                                        zindex : 9999,
+                                        centered : true,
+                                        html : 'this is mask'
+                                    } );
+                                }
+                            }
+                        }]
+                    },
                     store: {
                         type: 'tree',
 
@@ -38,8 +47,9 @@
                         },
 
                         proxy: {
-                            type: 'jsonp',
-                            url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
+                            type: 'ajax',
+                            url : '../../backend/test.php',
+                            // url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://feeds.feedburner.com/SenchaBlog',
                             reader: {
                                 type: 'json',
                                 rootProperty: 'responseData.feed.entries'
@@ -58,7 +68,14 @@
                             this.getDetailCard().setHtml(post.get('content'));
                         }
                     }
-                }
+                },
+
+                {
+                    xtype: 'panel',
+                    title: '我的任务',
+                    iconCls: 'home',
+                    cls: 'home'
+                },
             ]
         });
     }
